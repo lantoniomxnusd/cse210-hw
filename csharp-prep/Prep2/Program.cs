ce81a3221352420c1db83eb6b017a4864534b64a
lantoniomxnusd <luisoteacher@gmail.com>
2024-06-17 19:52:53 -0700

    Finished prep2 Core objectives

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage?");
+        string userGrade = Console.ReadLine();
+
+        string letter; 
+
+        int percent = int.Parse(userGrade);    
+
+        if (percent > 90 || percent == 90)
+        {
+            letter = ("A");
+        }
+
+        else if ((percent > 80 || percent == 80) && percent<90)
+        {
+            letter = ("B");
+        }
+
+        else if ((percent > 70 || percent == 70) && percent<80)
+        {
+            letter = ("C");
+        }
+
+        else if ((percent > 60 || percent == 60) && percent<70)
+        {
+            letter = ("D");
+        }
+
+        else
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine($"This is your grade: {letter}");
+
+        if (percent>70 || percent==70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+
+        else
+        {
+            Console.WriteLine ("Sorry, better luck next time!");
+        }
     }
+
+
 }
